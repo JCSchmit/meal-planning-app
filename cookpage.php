84e14795b8a489de17ee7ab97ecaff5ccdbc2600
Sosina325 <sosina.demelash@my.metrostate.edu>
2023-04-01 20:26:38 -0500

    connected with database

--- cookpage.php
+++ cookpage.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Meal Planning App</title>
     <meta charset="UTF-8">
@@ -16,32 +17,44 @@
             <a href="accountUserProfile.html"> <button class="circle-btn">Account</button></a>
         </div>
     </header>
-
     <!-- Main section -->
     <section>
         <!--Left panel-->
         <aside>
-            <label for="mealtype" class="meal_type">Meal type</label><br>
-            <select id="mealtype" name="mealtype" class="meal_type_menu">
-                <option value="blank">Select to filter</option>
-                <option value="plantbased">Plant based</option>
-                <option value="animalprotien">Animal protien</option>
-            </select><br><br>
+            <?php
+            require_once 'recipe.php';
 
-            <label for="ingredient_search" class="ingredient">Ingredient</label><br>
-            <input type="text" id="ingredient_search" name="ingredient_search" value="search" class="ingredient_search_box">
+            $servername = "localhost";
+            $username = "root";
+            $password = null;
+            $dbname = "mealplanningapp_db";
 
-            <image-container>
-                <img src="https://placehold.co/240X160" alt="description of the image" class="image">
-            </image-container>
+            // Create connection
+            $conn = new mysqli($servername, $username, $password, $dbname);
 
-            <image-container>
-                <img src="https://placehold.co/240X160" alt="description of the image" class="image">
-            </image-container>
+            // Check connection
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
+            ?>
+            <form method="POST" id="search">
+                <label for="mealtype" class="meal_type">Meal type</label><br>
+                <select id="mealtype" name="mealtype" class="meal_type_menu">
+                    <option value="blank">Select to filter</option>
+                    <option value="plantbased">Plant based</option>
+                    <option value="animalprotien">Animal protien</option>
+                </select><br><br>
 
-            <image-container>
-                <img src="https://placehold.co/240X160" alt="description of the image" class="image">
-            </image-container>
+                <label for="ingredient_search" class="ingredient">Ingredient</label><br>
+                <input type="text" id="ingredient_search" name="ingredient_search" class="ingredient_search_box">
+                <botton id="ingredientsearch" name="ingredientsearch" type="submit">
+                    Search</botton>
+            </form>
+
+            <?php
+            displayRecipeNames();
+            ?>
+            </form>
         </aside>
         <article>
             <div class="navigate">
@@ -60,70 +73,41 @@
             </div>
 
             <!--To do: add filename for action-->
-            <form method="POST" action="xxx.php">
-            <div class="recipe">
-                <div class="container">
-                    <div class="left">
-                        <div class="food_image">
-                            <img src="https://placehold.co/300X200" alt="Recipe Image">
+            <form method="POST">
+                <div class=" recipe">
+                    <div class="container">
+                        <div class="left">
+                            <div class="food_image">
+                                <img src="https://placehold.co/300X200" alt="Recipe Image">
+                            </div>
+                            <div class="ingredients">
+                                <div>
+                                    <label for="ingredient_name">Needed Ingredients </label><br>
+                                    <?php
+                                    displayIngredientRecipe();
+                                    ?>
+                                </div>
+                            </div>
                         </div>
-                        <div class="ingredients">
-                            <div>
-                                <label>Ingredients:</label>
-                                <table>
-                                    <thead>
-                                        <tr>
-                                            <th>Amount</th>
-                                            <th>Measurement</th>
-                                            <th id="ingredient">Ingredient</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr>
-                                            <td>1 </td>
-                                            <td>cup</td>
-                                            <td>Flour</td>
-                                        </tr>
-                                        <tr>
-                                            <td>1/2 </td>
-                                            <td>teaspoon</td>
-                                            <td>Salt</td>
-                                        </tr>
-                                        <tr>
-                                            <td>1/4 </td>
-                                            <td>cup</td>
-                                            <td>Sugar</td>
-                                        </tr>
-                                    </tbody>
-                                </table>
+                        <div class="right">
+                            <div class="instructions">
+                                <label><strong> Instructions For </strong></label><br>
+                                <?php
+                                displayRecipeInstruction();
+                                // Close the database connection
+                                $conn->close();
+                                ?>
                             </div>
                         </div>
                     </div>
-                    <div class="right">
-                        <div class="instructions">
-                            <label>Instructions:</label>
-                            <ol>
-                                <li>Preheat the oven to 350°F (180°C). Grease a baking sheet with non-stick spray.</li>
-                                <li>In a large bowl, combine the flour, salt, and sugar. Mix well.</li>
-                                <li>Add the butter and mix until crumbly.</li>
-                                <li>Add the egg and vanilla and mix until the dough comes together.</li>
-                                <li>Divide the dough into 12 equal pieces and roll each into a ball.</li>
-                                <li>Place the balls on the prepared baking sheet, spacing them 2 inches (5 cm) apart.
-                                </li>
-                                <li>Bake for 12-15 minutes, until the cookies are golden brown.</li>
-                                <li>Transfer the cookies to a wire rack and cool completely.</li>
-                            </ol>
-                        </div>
-                    </div>
                 </div>
-            </div>
             </form>
         </article>
     </section>
     <!-- Footer -->
-    <footer>
+    <!-- <footer>
         <p>&copy; 2023 Meal Planning App. All rights reserved.</p>
-    </footer>
+    </footer> -->
 </body>
 
 </html>
