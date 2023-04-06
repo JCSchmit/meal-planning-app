f908430dc8cfb93d73c4f48de9ab1950315fbfc5
Sosina325 <sosina.demelash@my.metrostate.edu>
2023-04-05 21:58:46 -0500

    login

--- mealpreplogin.php
+++ mealpreplogin.php
@@ -17,6 +17,8 @@ header {
 
 h1 {
     font-size: 2em;
+    /* color: #fff; */
+    color: #fff;
 }
 
 form {
@@ -39,20 +41,6 @@ input[type="password"] {
     width: 100%;
 }
 
-/* button[type="submit"] {
-    padding: 10px;
-    background-color: #F9A826;
-    color: #fff;
-    border: none;
-    border-radius: 5px;
-    font-size: 1em;
-    cursor: pointer;
-}
-
-button[type="submit"]:hover {
-    background-color: #FDE68A;
-} */
-
 button[type="submit"],
 button[type="button"] {
     padding: 10px;
@@ -70,23 +58,74 @@ button[type="button"]:hover {
 }
 
 a {
-    color: #FDE68A;
+    color: #F9A826;
     text-decoration: none;
 }
 
 footer {
     background-color: #F9A826;
-    padding: 5px;
+    padding: 10px;
     text-align: center;
-    position: absolute;
     bottom: 0;
-    width: 100%;
+    color: #fff;
+
+}
+
+footer p {
+    font-size: 0.8em;
 }
 
 main {
     flex: 1;
     /* Make the main element stretch to fill the remaining space */
     padding: 20px;
+    text-align: center;
+}
+
+.navbar {
+    width: 100%;
+    background-color: #FDE68A;
+    overflow: auto;
+}
+
+/* Navigation links */
+.navbar a {
+    float: left;
+    padding: 12px;
+    color: white;
+    text-decoration: none;
+    font-size: 17px;
+    width: 25%;
+    text-align: center;
+}
+
+/* Add a background color on mouse-over */
+.navbar a:hover {
+    background-color: #000;
+}
+
+/* Style the current/active link */
+/* .navbar a.active {
+background-color: black;
+} */
+
+
+@media screen and (max-width: 500px) {
+    .navbar a {
+        float: none;
+        display: block;
+        width: 100%;
+        text-align: left;
+    }
+}
+
+.navbar {
+    display: flex;
+    justify-content: space-between;
+}
+
+.navbar a {
+    flex: 1;
     text-align: center;
 }
 </style>
@@ -97,12 +136,11 @@ main {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700&display=swap" rel="stylesheet">
     <!-- <title>Meal Planning App - Login</title> -->
-    <link href="meal_planning_app.css" rel="stylesheet">
+    <!-- <link rel="stylesheet" href="Mealprep.css"> -->
 </head>
 <header>
-    <div class="menu">
-        <h1>Meal Planning App</h1>
-        <a href="index.html"> <button class="circle-btn">Home</button></a>
+    <h1>Meal Planning App</h1>
+    <a href="index.html"> <button class="circle-btn">Home</button></a>
     </div>
 </header>
 
@@ -112,16 +150,17 @@ main {
     </header>
     <main>
         <h2>Welcome back to the Meal Planning App!</h2>
+
         <form action="login_handler.php" method="POST">
             <label for="username-email">Username/Email:</label>
             <input type="text" id="username-email" name="username-email" required>
             <label for="password">Password:</label>
             <input type="password" id="password" name="password" required>
             <?php
-      if (isset($_GET['error'])) {
-        echo '<p class="error">Invalid username/email or password</p>';
-      }
-      ?>
+        if (isset($_GET['error'])) {
+            echo '<p class="error">Invalid username/email or password</p>';
+        }
+        ?>
             <button type="submit" name="submit">Sign In</button>
 
             <button type="button" onclick="location.href='resetPassword.php'">Reset Password</button>
